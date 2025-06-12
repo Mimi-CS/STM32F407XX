b06ac3cae7c62b2acc8521c7fba639962cc77267
Mimi <mimics080@gmail.com>
2025-06-12 08:24:06 +0530

    SPI_Init fn

--- drivers/src/stm32f4xx_spi_driver.c
+++ drivers/src/stm32f4xx_spi_driver.c
@@ -40,6 +40,32 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx,uint8_t EnorDi)
 
 void SPI_Init(SPI_Handle_t *pSPIHandle)
 {
+	uint32_t tempreg = 0;
+
+	/* Config of CR1 reg*/
+
+	/* 1. SPI_DeviceMode , 2nd bit of SPI-CR1 reg */
+
+	tempreg |= pSPIHandle->SPIConfig.SPI_DeviceMode<<2;
+
+	/* 2.SPI_BusConfig ,15th bit of SPI-CR1reg */
+
+	if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+	{
+		/* 15 th bit should be 0 ,ie;2 lines unidirectional mode*/
+		tempreg &= ~(1<<15);
+	}
+	else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+	{
+		/* 15 th bit should be 1 ,ie;one line bidirectional mode*/
+		tempreg |= (1<<15);
+	}
+	else if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
+	{
+		/* 15 th bit should be 0 and 10 th bit 1*/
+		tempreg &= ~(1<<15);
+		tempreg |= (1<<10);
+	}
 
 }
 
